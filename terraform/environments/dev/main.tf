beb5edbd7768e0725a055d5f91ecd49d9a4f7577
Chinmaya Prasad Sahoo <chinmayash11@gmail.com>
2026-05-10 14:42:40 +0530

    initial commit

--- terraform/environments/dev/main.tf
+++ terraform/environments/dev/main.tf
@@ -7,11 +7,11 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.0"
+      version = "5.100.0"
     }
     tls = {
       source  = "hashicorp/tls"
-      version = "~> 4.0"
+      version = "4.2.1"
     }
   }
 
